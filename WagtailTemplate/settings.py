33e7ca2085623c473ecdc02a7046df81ef163f0d
Chris Killian <killiantheelder@gmail.com>
2020-11-22 19:46:44 -0700

    Modifying static files settings

--- WagtailTemplate/settings.py
+++ WagtailTemplate/settings.py
@@ -170,6 +170,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "media"),
 ]
 
 # Access this by running tail -f /tmp/debug.log
---
+++ _oldstaticfiles/ours/js/navigate.ff0f95c38c19.js
@@ -0,0 +1,5 @@
+function navigateTo(url) {
+    let base_url = "https://dungeomancer.herokuapp.com";
+    console.log(base_url + url)
+    window.location.href = base_url + url
+}
