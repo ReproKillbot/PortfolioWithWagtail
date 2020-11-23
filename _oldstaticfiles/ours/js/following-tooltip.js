e116c78e9174e6cfdbf22b7b4be0c1f19a1c631e
Chris Killian <killiantheelder@gmail.com>
2020-11-22 19:52:04 -0700

    Putting back in semantic wttff

---
+++ _oldstaticfiles/ours/js/following-tooltip.js
@@ -0,0 +1,12 @@
+//
+// let tooltipSpan = document.getElementById('tooltip-span');
+// let oneSecHover = false;
+//
+// $('.tooltipSpan').hover(){
+//
+//         let x = e.clientX,
+//             y = e.clientY;
+//         tooltipSpan.style.top = (y + 20) + 'px';
+//         tooltipSpan.style.left = (x + 20) + 'px';
+//
+// }
